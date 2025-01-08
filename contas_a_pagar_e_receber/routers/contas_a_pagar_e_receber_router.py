9fae2633142b7b6323492604ccee6d9452cbebd4
AnaLuisaComper <comperana@gmail.com>
2025-01-08 18:56:16 -0300

    Estrutura da API

---
+++ contas_a_pagar_e_receber/routers/contas_a_pagar_e_receber_router.py
@@ -0,0 +1,32 @@
+from decimal import Decimal
+from typing import List
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+router= APIRouter(prefix="/contas-a-pagar-e-receber")
+
+class ContaPagarReceberResponse(BaseModel):
+    id: int
+    descricao: str
+    valor: Decimal
+    tipo: str # PAGAR, RECEBER
+    
+class ContaPagarReceberRequest(BaseModel):
+    descricao: str
+    valor: Decimal
+    tipo: str # PAGAR, RECEBER
+
+
+@router.get("/", response_model=List[ContaPagarReceberResponse])
+def listar_contas():
+    return[
+        ContaPagarReceberResponse(id = 1, descricao = "Aluguel", valor = 10, tipo="pagar"),
+        ContaPagarReceberResponse(id = 2, descricao = "Internet", valor = 10, tipo="pagar"),
+        ContaPagarReceberResponse(id = 3, descricao = "Salario", valor = 1000000000, tipo="receber"),
+    ]
+    
+@router.post("/", response_model=ContaPagarReceberResponse, status_code=201)    
+def criar_conta(conta: ContaPagarReceberRequest):
+    return ContaPagarReceberResponse( 
+            id = 1, descricao = conta.descricao, valor = conta.valor, tipo=conta.tipo
+            )
